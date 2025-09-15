4614a86fa3da3da1e9ee8b46a62afd8126fae03c
Kanoa Parker <kanparker@hmc.edu>
2025-09-15 16:58:46 -0700

    stop clamp

--- fpga/e155_lab3/source/impl_1/stop_clamp.sv
+++ fpga/e155_lab3/source/impl_1/stop_clamp.sv
@@ -1,39 +1,99 @@
 module stop_clamp(
-	input logic [3:0]cin,
-	output logic stop,
-	output logic [3:0]clamps,
-	output logic [3:0] cout
+	input logic [3:0] cin,
+	output logic [3:0] clamps,
+	output logic [3:0] cout,
+	output logic stop
 );
-
+	logic [3:0]cin_cleaned;
+	
 	always_comb
-	begin
-		stop = (cin[0]|cin[1])|(cin[2]|cin[3]);
+		begin
+			stop = |cin; // |cin;
+			/*	
+			// if (cin==4'b0) begin clamps, cout
+			// else begin clamps = cin, cout = cin end
+			case(cin)
+				4'b0001: cin_cleaned = 4'b0001;
+				4'b001?: cin_cleaned = 4'b0010;
+				4'b01??: cin_cleaned = 4'b0100;
+				4'b1???: cin_cleaned = 4'b1000;
+			endcase
+			// cintmp -> cin, casez 1???, 01??, ...
+			if(cin==4'b0) begin
+				clamps = 4'b1111;
+				cout =4'b0;
+				end
+			else begin
+				clamps = cin_cleaned;
+				cout = cin_cleaned;
+				end
 			
-		case(cin[3:0])
-			4'b0000: begin
-				clamps[3:0] = 4'b1111;
-				cout[3:0] = 4'b0000;
-				end
-			4'b0001: begin
-				clamps[3:0] = 4'b0001;
-				cout[3:0] = 4'b0001;
-				end
-			4'b001x: begin
-				clamps[3:0] = 4'b0010;
-				cout[3:0] = 4'b0010;
-				end
-			4'b01xx: begin
-				clamps[3:0] = 4'b0100;
-				cout[3:0] = 4'b0100;
-				end
-			4'b1xxx: begin
-				clamps[3:0] = 4'b1000;
-				cout[3:0] = 4'b1000;
-				end
-			default: begin
-				clamps[3:0] = 4'b1111;
-				cout[3:0] = 4'b0000;
-				end
-		endcase
-	end
+			*/	
+			case(cin[3:0])
+				4'b0000: begin
+					clamps[3:0] = 4'b1111;
+					cout[3:0] = 4'b0000;
+					end
+				4'b0001: begin
+					clamps[3:0] = 4'b0001;
+					cout[3:0] = 4'b0001;
+					end
+				4'b0010: begin
+					clamps[3:0] = 4'b0010;
+					cout[3:0] = 4'b0010;
+					end
+				4'b0011: begin
+					clamps[3:0] = 4'b0010;
+					cout[3:0] = 4'b0010;
+					end
+				4'b0100: begin
+					clamps[3:0] = 4'b0100;
+					cout[3:0] = 4'b0100;
+					end
+				4'b0101: begin
+					clamps[3:0] = 4'b0100;
+					cout[3:0] = 4'b0100;
+					end
+				4'b0110: begin
+					clamps[3:0] = 4'b0100;
+					cout[3:0] = 4'b0100;
+					end
+				4'b0111: begin
+					clamps[3:0] = 4'b0100;
+					cout[3:0] = 4'b0100;
+					end
+				4'b1000: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1001: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1010: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1011: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1100: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1101: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1110: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+				4'b1111: begin
+					clamps[3:0] = 4'b1000;
+					cout[3:0] = 4'b1000;
+					end
+			endcase
+		end
 endmodule
--- fpga/e155_lab3/source/impl_1/stop_clamp_tb.sv
+++ fpga/e155_lab3/source/impl_1/stop_clamp_tb.sv
@@ -4,19 +4,18 @@
 module stop_clamp_tb();
 	logic clk, reset;
 	logic [31:0] vectornum,errors;
-	logic [13:0] testvectors[10000:0];
-	logic stop ,stop_expected;
-	logic [3:0] cin, clamps, clamps_expected, cout, cout_expected;
+	logic [12:0] testvectors[10000:0];
+	logic stop,stop_expected;
+	logic [3:0] cin,clamps,clamps_expected,cout,cout_expected;
 	
 	// instantiate device under test
-	stop_clamp dut(cin,stop,clamps,cout);
+	stop_clamp dut(cin,clamps,cout,stop);
 	
 	// at start of test, load vectors and pulse reset
 	initial
 		begin
-			$readmemb("stop_clamp.tv", testvectors, 0, `N_TV - 1);
+			$readmemb("stop_clampper.tv", testvectors, 0, `N_TV - 1);
 			vectornum = 0; errors = 0; reset = 1; #22; reset = 0;
-			
 		end
 		
 	always
@@ -27,36 +26,38 @@ module stop_clamp_tb();
 		// apply test vectors on rising edge of clk
 	always @(posedge clk)
 		begin
-			#1; {cin,clamps_expected,cout_expected, stop_expected} = testvectors[vectornum];
-			
+			#1; {cin,clamps_expected,cout_expected,stop_expected} = testvectors[vectornum];
+			$display("%b %b %b %b", cin, clamps_expected, cout_expected,stop_expected);
 		end
 
 // check results on falling edge of clk
 	always @(negedge clk)
 		if (~reset) begin // skip during reset
-			if(cout !=cout_expected) begin
-					$display("Error: inputs = %b", {cin});
+			if(cout !==cout_expected) begin
+					$display("Error: inputs = %b", cin);
 					$display(" Cout outputs = %b (%b expected)", cout, cout_expected);
 					errors = errors + 1;
 				end;
-			if(clamps !=clamps_expected) begin
-					$display("Error: inputs = %b", {cin});
+			if(clamps !==clamps_expected) begin
+					$display("Error: inputs = %b", cin);
 					$display(" Clamps outputs = %b (%b expected)", clamps, clamps_expected);
 					errors = errors + 1;
 				end;
-			if(stop !=stop_expected) begin
-					$display("Error: inputs = %b", {cin});
+			if(stop !==stop_expected) begin
+					$display("Error: inputs = %b", cin);
 					$display(" Stop outputs = %b (%b expected)", stop, stop_expected);
 					errors = errors + 1;
 				end;
+				
+			assign vectornum = vectornum +1;
 			
-			if (testvectors[vectornum] === 14'bx) begin
+			if (testvectors[vectornum] === 13'bx) begin
 				$display("%d tests completed with %d errors", vectornum, errors);
 				$stop;
 			end
 				
 			
-			assign vectornum = vectornum +1;
+			
 		
 			
 		end
